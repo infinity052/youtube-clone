1dc6ddf66cc3f6d343b75030187e8eb45befb00b
Ayush Jain <aayushhjain23@gmail.com>
2020-11-17 00:06:29 +0530

    Updates

---
+++ precache-manifest.ad39577a0bae60bf4b33b732d3cb5b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa55af17cd06e44f8ef58345e20b6a7",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "88a11519a0bc61935bbd",
+    "url": "/youtube-clone/static/css/main.ebf9682d.chunk.css"
+  },
+  {
+    "revision": "94c3430f7bd9374a0059",
+    "url": "/youtube-clone/static/js/2.b254c515.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/youtube-clone/static/js/2.b254c515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a11519a0bc61935bbd",
+    "url": "/youtube-clone/static/js/main.d5995f95.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  },
+  {
+    "revision": "262a9cb882d14a425253582d56464e24",
+    "url": "/youtube-clone/static/media/logo.262a9cb8.svg"
+  }
+]);
